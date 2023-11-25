8bb0aefc37b2c95bb4bf41c6a8d33d449d7d1ae3
Prasheel Vellanki <prasheelvellanki@gmail.com>
2023-11-25 17:17:18 -0500

    entries

--- frontend/src/Pages/entries/Entries.tsx
+++ frontend/src/Pages/entries/Entries.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/Button';
 import InfoIcon from '@mui/icons-material/Info';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Switch from '@mui/material/Switch';
 import './Entries.css'; 
+import { Icon } from "@mui/material";
+import IOSSwitch from "../../Assets/IOSToggle";
 
 interface IEntry {
     date: string,
@@ -32,25 +37,37 @@ export const samEntry: IEntry = {
     content: 'Sample helo content',
 };
 
+
+
 const EntryCard: React.FC<IEntry> = ({date, content}) => {
     return (
-      <div className="w-full h-2/6 pt-3 m-auto"> 
-      <div className="w-full h-full m-auto flex rounded-3xl border-4 box-shadow">
-      <div className="w-3/12 flex p-2 items-center justify-center">
-          {date}
+      <div className="w-full h-1/6 pt-3 m-auto">
+        <div className="w-full h-full m-auto flex rounded-3xl border-4 box-shadow">
+          <div className="w-3/12 flex p-2 items-center justify-center">
+            {date}
+          </div>
+          <div className="w-7/12 flex items-center justify-center">
+            {content}
+          </div>
+          <div className="w-2/12 flex items-center justify-center">
+            <div className="btn drop-shadow-none">
+                <Button
+                style={{
+                    borderRadius: 10,
+                    backgroundColor: "#b08968",
+                    padding: "8px 16px",
+                    fontSize: "12px"
+                }}
+                disableElevation={true}
+                variant="contained"
+                endIcon={<InfoIcon />}
+                className="w-full h-full hover:bg-red-400"
+                >
+                  <div className="normal-case">View</div>
+                </Button>
+            </div>
+          </div>
         </div>
-        <div className="w-7/12 flex items-center justify-center">
-          {content}
-        </div>
-        <div className="w-2/12 flex items-center justify-center">
-        <div className=" rounded-md shadow-md">
-        <Button variant="contained" color="primary"  endIcon={<InfoIcon />} className="w-full lowercase h-full">
-          <div className="normal-case">View</div>
-        </Button>
-      </div>
-        </div>
-      </div>
-        
       </div>
     );
 }
@@ -60,22 +77,32 @@ const Entries: React.FC<IEntries> = ({entries}) => {
     return (
       <div className="w-full h-screen ">
         <div className="row h-2/6 flex w-full">
-            <div className="h-full w-10/12 m-auto flex items-end">
-                <div className="text-left font-sans pl-4 font-bold text-6xl">
-                    Past Entries
-                </div>
+          <div className="h-full w-1/12 flex"></div>
+
+          <div className="h-full w-10/12 flex items-end">
+            <div className="text-left font-sans pl-4 font-bold text-5xl pb-3">
+              Past Entries
             </div>
+            <div className="flex-1" />
+            <div className="pb-3">
+              <IOSSwitch />
+            </div>
+            <div className="btn ">
+                <IconButton>
+                  <AddCircleOutlineIcon style={{ fontSize: "3rem", color: "#65C466" }} />
+                </IconButton>
+              </div>
+          </div>
+          <div className="h-full w-1/12 flex"></div>
         </div>
         <div className="row h-4/6 pb-2 flex w-full place-content-center">
           <div className="w-10/12 h-full box-content border-2 border-gray-200 rounded-3xl overflow-y-hidden">
             <div className="w-full h-full m-auto overflow-y-auto">
-
-            
-            <div className="w-10/12 h-full m-auto mb-5">
-            {entries.map((entry, index) => (
-              <EntryCard key={index} {...entry} />
-            ))}
-            </div>
+              <div className="w-11/12 h-full m-auto mb-5">
+                {entries.map((entry, index) => (
+                  <EntryCard key={index} {...entry} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/Pages/landing/Landing.tsx
+++ frontend/src/Pages/landing/Landing.tsx
@@ -10,19 +10,20 @@ const Landing: React.FC<{}> = () => {
             <img src={login_image} className="drop-shadow-2xl"></img>
           </div>
         </div>
-        <div className="column w-7/12 py-8 grid grid-cols-1">
-          <div className="row h-4/12 p-8 pl-0 flex items-center">
-            <span className="m-0 font-bold text-8xl">
+        <div className="column w-7/12 grid grid-cols-1">
+        <div className="row h-2/12 p-8 flex"></div>
+          <div className="row h-6/12 p-8 pl-0 pb-0 flex items-center">
+            <div className="m-0 font-bold text-8xl items-end">
                 <text className="text-green-grey stroke-text">Full</text><em className="text-palette-green stroke-text">mood</em>
-                </span>
+                </div>
           </div>
-          <div className="row h-4/12 p-8 pr-12">
+          <div className="row  pr-12 items-center ">
             <div className="text-lg font-semibold text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
             </div>
-          <div className="row h-4/12 p-8"></div>
+          <div className="row h-2/12 p-8 flex"></div>
         </div>
       </div>
     );
